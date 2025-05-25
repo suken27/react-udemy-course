76a4c00d87adc8edf094ec73bd3e32dc42f352ec
suken27 <suken2727@gmail.com>
2025-05-25 18:38:51 +0200

    Add content up to point 52 in the course

--- essentials/src/App.tsx
+++ essentials/src/App.tsx
@@ -1,10 +1,16 @@
 import CoreConcept from './components/CoreConcept';
 import Header from './components/Header/Header';
+import TabButton from './components/TabButton';
 import { CORE_CONCEPTS } from './data';
 
 
 
 function App() {
+
+  function handleSelect(identifier: string): void {
+    console.log(identifier);
+  }
+  
   return (
     <div>
       <Header />
@@ -18,7 +24,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
